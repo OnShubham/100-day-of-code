82d9af825afd1edbb971f246f2ba0a2115d5a9bb
OnShubham <shubhamkharche3005@gmail.com>
2024-04-14 13:38:12 +0530

    day 9

--- calculateAverage.js
+++ calculateAverage.js
@@ -1,7 +1,9 @@
+// Day 9
+
 const calculateAverage = (arr) => {
   let total = arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
   console.log(total);
-    return total.toFixed(1);
+  return total.toFixed(1);
 };
 
 console.log(calculateAverage([76, 60, 83, 100, 78])); // 79.4
