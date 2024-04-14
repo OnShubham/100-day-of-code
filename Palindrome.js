bee86681ac6115bc117856dbf8631daa51f84830
OnShubham <shubhamkharche3005@gmail.com>
2024-04-14 12:52:19 +0530

    palindrome

---
+++ Palindrome.js
@@ -0,0 +1,9 @@
+const isPalindrome = (str) => {
+  str = str.toLowerCase();
+  let reversedStr = str.split("").reverse().join("");
+  return reversedStr === str;
+};
+
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("table")); // false
+console.log(isPalindrome("Anna")); // true
