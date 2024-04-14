c0507367248702dd9cdf1ba7dcc88b27d73922fd
OnShubham <shubhamkharche3005@gmail.com>
2024-04-14 13:12:20 +0530

    factroial

--- findTheMaxNum.js
+++ findTheMaxNum.js
@@ -3,3 +3,5 @@ const findMax = (arr) => {
 };
 
 console.log(findMax([1, 2, 3, 4, 5]));
+
+//math function : max method & min method & pow method & sqrt method & random method & round method & floor method & ceil method & abs method & sin method & cos method & tan method & asin method & acos method & atan method & log method & exp method & PI & E & SQRT2 & SQRT1_2 & LN2 & LN10 & LOG2E & LOG10E
